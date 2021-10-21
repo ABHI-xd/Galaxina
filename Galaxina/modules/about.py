6aa716d1b7c364eccfe39e32fa3a5692150b2029
NitricXd <92805675+NitricXd@users.noreply.github.com>
2021-10-21 09:45:20 +0530

    💛 GalaXina 💛

---
+++ Galaxina/modules/about.py
@@ -0,0 +1,14 @@
+__help__ = """
+** Galaxina - A Powerful Telegram Group Manager 🎶 **
+
+Powerful Abilities
+
+• Group Voice Chat Music Play ❤️
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+Developers [𓆩Nɪᴛʀɪᴄ'Xd𓆪🇮🇳](https://t.me/KuziYo)
+"""
+__mod_name__ = "About"
