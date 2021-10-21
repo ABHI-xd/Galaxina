2065653cb140656db2aa6ffaaff913d8070288d6
NitricXd <92805675+NitricXd@users.noreply.github.com>
2021-10-21 07:56:02 -0700

    Update config.py

--- Galaxina/config.py
+++ Galaxina/config.py
@@ -14,21 +14,21 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    API_ID = 8479425  # integer value, dont use ""
+    API_HASH = "5cd82c418012e2173e85728a0782fd5b"
+    TOKEN = "2034558488:AAFCVz6rjcXEu1Opzyfny2oSMIuphk2SUlQ"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 2042990675  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "KuziYo"
+    SUPPORT_CHAT = "Galaxina_Support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001627826970
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001627826970
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = "postgres://pmtkwwjt:6N-V31MqjtGPXjT2i4VsJZVjGjdFrHaA@fanny.db.elephantsql.com/pmtkwwjt"  # needed for any database modules
     REDIS_URI = " "
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
@@ -40,14 +40,14 @@ class Config(object):
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    DRAGONS = get_user_list("elevated_users.json", "2042990675 2062518159 2067182444")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    DEV_USERS = get_user_list("elevated_users.json", "2042990675 2062518159 2067182444")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "supports")
+    DEMONS = get_user_list("elevated_users.json", "2042990675 2062518159 2067182444")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "tigers")
-    WOLVES = get_user_list("elevated_users.json", "whitelists")
+    TIGERS = get_user_list("elevated_users.json", "2042990675 2062518159 2067182444")
+    WOLVES = get_user_list("elevated_users.json", "2042990675 2062518159 2067182444")
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
@@ -65,7 +65,7 @@ class Config(object):
     WALL_API = (
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
-    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    AI_API_KEY = "ea6f630d3e93996383af66584bc4fe0351a2c623dd4dd3c1fe777dab1bdbda622073234fdc4e4d44f7f493d098872e984632ca2b2f05e1502533cf852f76189a"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
