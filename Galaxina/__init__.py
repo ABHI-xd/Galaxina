46c4b433d776bb635668d7b6eb658b07a2f68737
NitricXd <92805675+NitricXd@users.noreply.github.com>
2021-10-21 22:26:50 +0530

    Update __init__.py

--- Galaxina/__init__.py
+++ Galaxina/__init__.py
@@ -89,13 +89,13 @@ if ENV:
     REDIS_URL = os.environ.get("REDIS_URL")
     IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "")
 
     
 
     try:
         TEMP_DOWNLOAD_DIRECTORY = set(
-            int(x) for x in os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./").split()
+            int(x) for x in os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "").split()
         )
     except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
@@ -174,7 +174,7 @@ else:
     INFOPIC = Config.INFOPIC
     try:
         TEMP_DOWNLOAD_DIRECTORY = set(
-            int(x) for x in os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./").split()
+            int(x) for x in os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "").split()
         )
     except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
